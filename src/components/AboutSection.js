ae1daf8867d5be1fc74d574afa182547ca62fbd6
Vaishnavi Aswale <48441026+Vaishnavi-Aswale@users.noreply.github.com>
2023-09-26 18:59:49 -0700

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -80,7 +80,7 @@ export default function AboutSection() {
                     heading="Hello World!"
                 />
                 <PText>
-                I am Vaishnavi Aswale, a tech enthusiast with a Bachelor's degree in Information Technology, graduated in 2020. Currently pursuing a Master's in Information Technology at ASU, I've amassed 2.9 years of professional experience. I began as a Software Development Engineer at Eduvanz, a fintech startup, and later transitioned into the role of Junior Data Scientist.
+                I&apos;m Vaishnavi Aswale, a tech enthusiast with a Bachelor&apos;s degree in Information Technology, graduated in 2020. Currently pursuing a Master&apos;s in Information Technology at ASU, I&apos;ve amassed 2.9 years of professional experience. I began as a Software Development Engineer at Eduvanz, a fintech startup, and later transitioned into the role of Junior Data Scientist.
                 </PText>
                 <div className="aboutSection__buttons">
                     <Button btnText="Profile" btnLink="/profile" />
