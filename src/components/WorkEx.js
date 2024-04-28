701dfbbe316e4c95914a8f7d2f67bab45dfebdbe
Vaishnavi Aswale <48441026+Vaishnavi-Aswale@users.noreply.github.com>
2024-04-28 13:38:56 -0700

    Update WorkEx.js

--- src/components/WorkEx.js
+++ src/components/WorkEx.js
@@ -33,20 +33,20 @@ export default function WorkEx() {
               icon={<MdDesktopMac />}
               title="Software Developer"
               subtitle="Eduvanz Financing Pvt Ltd- Nov 2020- Sept 2020"
-              desc="I developed a customer data verification application using HTML, CSS, JavaScript, and PHP, resulting in a 65% revenue increase by streamlining loan applications. I improved project functionality by contributing to Laravel and CodeIgniter frameworks, optimized heavy SQL queries by 79%, and integrated third-party APIs for efficient collaboration using Git and JIRA."
+              desc=""
             />
             
             <WorkItems
               icon={<MdPeopleOutline />}
               title="Junior Data Scientist"
               subtitle="Eduvanz Financing Pvt Ltd- Sept 2022- July 2023"
-              desc="I leveraged advanced SQL queries to analyze loan default predictions, achieving a 25% accuracy improvement through data preprocessing and Random Forest Algorithm. I spearheaded the creation of a Power BI dashboard, resulting in a 10% revenue boost through data-driven insights. Designed, validated, and implemented Python and SQL data pipelines for precise analysis and reporting in business operations."
+              desc=""
             />
             <WorkItems
               icon={<MdCloudQueue />}
               title="Cloud Intern"
               subtitle="iFuture Technologies -2 months"
-              desc="I established a client-server setup in a private cloud, linking devices via LAN, with one as the server and the rest as clients, all on Microsoft Server OS. I also managed the deployment of a cloud app, ensuring seamless access and performance on client devices."
+              desc=""
             />
           </div>
         </div>
